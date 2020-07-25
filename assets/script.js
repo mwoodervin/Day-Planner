9ed37c51ef56620fe6493f3844ccfa4529892681
Mary Wood <mwoodervin@gmail.com>
2020-07-24 21:08:47 -0400

    Save button saves to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,98 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     // Set Global Variables
-
     let todoDiv;
+    // let now;
+    let inputHour;
 
-    // set date in header
+    // Set date in header
     let today = moment();
-    
-    $("#currentDay").text(today.format("LL"));
-    // Create Calendar
-    // how do I make the hours work?
+    $("#currentDay").text(today.format("dddd | LL | H mm a"));
+    // now = moment().hour(6);
 
-    function createCalendar(date) {
+    // Function to Create Calendar
 
-        date = moment().hour(6);
+    function createCalendar(now) {
+
+        now = moment().hour(6);
 
         for (let i = 0; i < 18; i++) {
-            
-            let rowDiv = $("<div>").addClass("row");
 
-            let timeBlock = $("<div>").addClass("col-1 hour time-block d-flex align-items-center justify-content-center").text(date.format("h a"));
-            
+            var todoItem = localStorage.getItem(i);
+            if (!todoItem) {
+                todoItem = "";
+            }
+
+            let rowSetDiv = $("<div>").addClass("row");
+
+            let timeBlock = $("<div>").addClass("col-1 hour time-block d-flex align-items-center justify-content-center").text(now.format("h a"));
+            // i here corresponds to 0-18; data value added to element with a data value
+            timeBlock.attr("data-hour", i);
+
             todoDiv = $("<textarea>").addClass("col-10 textarea");
-            
+
             let saveDiv = $("<button>").addClass("col-1 btn saveBtn");
             saveBtnIcon = $("<i>").addClass("fas fa-save fa-3x d-flex align-items-center justify-content-center");
 
-            date.add(1, "hour");
-            
-            $(".container").append(rowDiv.append(timeBlock,todoDiv,saveDiv.append(saveBtnIcon)));
+            todoDiv.text(todoItem);
 
-            if (today.isAfter(date, "hour")) {
+            $(".container").append(rowSetDiv.append(timeBlock, todoDiv, saveDiv.append(saveBtnIcon)));
+
+            if (today.isAfter(now, "hour")) {
                 todoDiv.addClass("past");
-            } else if (today.isBefore(date, "hour")) {
+            } else if (today.isBefore(now, "hour")) {
                 todoDiv.addClass("future");
             } else {
                 todoDiv.addClass("present");
             }
+            // highlightTime();
+
+            now.add(1, "hour");
+
         }
     }
 
+    // Function to store calendar to-do items
+
+    function storeToDo() {
+        let inputText = $(this).parent().parent().find("textarea")[0].value;
+        let inputHour = $(this).parent().parent().find(".hour")[0].dataset.hour;
+
+        
+        localStorage.setItem(inputHour, inputText);
+    }
+
+    // console.log(moment().hour());
+    // console.log(now.format("h"));
+    // console.log($(".hour").val());
+
+// if (!moment().hour() === now) {
+//     console.log("no match");
+
+    // function hourHighlightUpdate() {
+    //     // let currentHour = today;
+    //     // let currentTimeBlock = now;
+    //     // if (!moment().hour() == now) {
+    //     //     console.log("no match");
+    //         //     if (today.isAfter(now, "hour")) {
+    //         //         todoDiv.addClass("past");
+    //         //     } else if (today.isBefore(now, "hour")) {
+    //         //         todoDiv.addClass("future");
+    //         //     } else {
+    //         //         todoDiv.addClass("present");
+    //         //     }
+    //     }
+    // })
+
+    // CLICK EVENTS
+    // To load calendar when page is opened
     $(window).on("load", createCalendar());
 
-    // // function to store input
-    // function storeToDo() {
-    //     console.log(todoDiv.innerHTML);
-    //     localStorage.setItem("todoList", JSON.stringify(todoDiv.text)); 
-    // };
-
-    // // function to retrieve input
-
-    // function getToDo() {
-    //     localStorage.getItem("todoList", JSON.parse(todoDiv.text));
-    // };
-
-    // // on-click event to save todoItems to local storage
-    // $(".btn").click(storeToDo);
+    // To store text input when "save" icon is clicked
+    $(document).on("click", "i", storeToDo);
 
 });
+
+// need to add a function to check the time and refresh page if rolled to a new hour
+// this refresh needs to display saved items
 
