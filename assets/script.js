1e9f7c5054efb3de965bbf6370b85b74d123373f
Mary Wood <mwoodervin@gmail.com>
2020-07-25 07:33:52 -0400

    Add clear button, clears item from local storage

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,14 @@ $(document).ready(function () {
 
     // Set Global Variables
     let todoDiv;
+    let text = $(".textarea");
+
     // let now;
     let inputHour;
+    let inputText;
+    let timeblock;
+    // let inputText = $(this).parent().parent().find("textarea")[0];
+    // let inputHour = $(this).parent().parent().find(".hour")[0].dataset.hour;
 
     // Set date in header
     let today = moment();
@@ -25,18 +31,19 @@ $(document).ready(function () {
 
             let rowSetDiv = $("<div>").addClass("row");
 
-            let timeBlock = $("<div>").addClass("col-1 hour time-block d-flex align-items-center justify-content-center").text(now.format("h a"));
-            // i here corresponds to 0-18; data value added to element with a data value
+            timeBlock = $("<div>").addClass("col-1 hour time-block d-flex align-items-center justify-content-center").text(now.format("h a"));
+
             timeBlock.attr("data-hour", i);
 
             todoDiv = $("<textarea>").addClass("col-10 textarea");
 
             let saveDiv = $("<button>").addClass("col-1 btn saveBtn");
-            saveBtnIcon = $("<i>").addClass("fas fa-save fa-3x d-flex align-items-center justify-content-center");
+            saveBtnIcon = $("<i>").addClass("fas fa-save fa-2x d-flex align-items-top justify-content-center");
+            clearBtnIcon = $("<i>").addClass("fas fa-backspace fa-2x d-flex align-items-bottom justify-content-center");
 
             todoDiv.text(todoItem);
 
-            $(".container").append(rowSetDiv.append(timeBlock, todoDiv, saveDiv.append(saveBtnIcon)));
+            $(".container").append(rowSetDiv.append(timeBlock, todoDiv, saveDiv.append(saveBtnIcon,clearBtnIcon)));
 
             if (today.isAfter(now, "hour")) {
                 todoDiv.addClass("past");
@@ -45,11 +52,11 @@ $(document).ready(function () {
             } else {
                 todoDiv.addClass("present");
             }
-            // highlightTime();
 
             now.add(1, "hour");
 
         }
+
     }
 
     // Function to store calendar to-do items
@@ -57,12 +64,26 @@ $(document).ready(function () {
     function storeToDo() {
         let inputText = $(this).parent().parent().find("textarea")[0].value;
         let inputHour = $(this).parent().parent().find(".hour")[0].dataset.hour;
-
         
         localStorage.setItem(inputHour, inputText);
     }
 
-    // console.log(moment().hour());
+    function clearToDo() {
+        let inputText = $(this).parent().parent().find("textarea")[0].value;
+        let textBox = $(this).parent().parent().find("textarea")[0];
+        let inputHour = $(this).parent().parent().find(".hour")[0].dataset.hour;
+        localStorage.removeItem(inputHour);
+        console.log(inputText);
+        text.val("");
+    }
+    //     console.log(inputText);
+    //         if(inputText) {
+    //         inputText.innerHTML = "";
+    //         }
+    // }
+
+// console.log(text.val);
+//     console.log(moment().hour());
     // console.log(now.format("h"));
     // console.log($(".hour").val());
 
@@ -89,7 +110,11 @@ $(document).ready(function () {
     $(window).on("load", createCalendar());
 
     // To store text input when "save" icon is clicked
-    $(document).on("click", "i", storeToDo);
+    $(document).on("click", ".fa-save", storeToDo);
+
+    // To clear text input when "clear" icon is clicked
+    $(document).on("click", ".fa-backspace", clearToDo);
+
 
 });
 
